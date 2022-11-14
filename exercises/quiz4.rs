3b9882339cbdd93abf7461b75edd36ebc9637915
Yap Miracle <2049829115@qq.com>
2022-11-14 19:38:53 +0800

    update

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -6,7 +6,23 @@
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
 // I AM NOT DONE
-
+macro_rules! my_macro{
+    ($a:expr)=>{
+        let mut a = "Hello ";
+        let mut b = $a.to_string();
+        return a+b
+    }
+}
+// macro_rules! add{
+//     // macth like arm for macro
+//        ($a:expr,$b:expr)=>{
+//     // macro expand to this code
+//            {
+//    // $a and $b will be templated using the value/variable provided to macro
+//                $a+$b
+//            }
+//        }
+//    }
 #[cfg(test)]
 mod tests {
     use super::*;
